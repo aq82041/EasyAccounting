65937d447fb64e65ec183d98fc3bab5bd4f2ef14
anqi <18742024663@163.com>
2020-05-17 15:50:08 +0800

    全局组件Nav

---
+++ src/views/Statistics.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Statistics.vue
+        <Nav/>
+    </div>
+</template>
+
+<script lang="ts">
+
+    export default {
+        name: 'Statistics',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
