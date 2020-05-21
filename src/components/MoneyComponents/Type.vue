225cb4095b26b4429d1219fd235d638d2e0c95f6
anqi <18742024663@163.com>
2020-05-21 19:39:21 +0800

    TS实现Type

--- src/components/MoneyComponents/Type.vue
+++ src/components/MoneyComponents/Type.vue
@@ -2,52 +2,57 @@
     <div>
         <ul class="types">
             <li :class="type==='-' && 'selected'"
-            @click="selectType('-')">支出</li>
+                @click="selectType('-')">支出
+            </li>
             <li :class="type==='+' && 'selected'"
-            @click="selectType('+')">收入</li>
+                @click="selectType('+')">收入
+            </li>
         </ul>
     </div>
 </template>
 
-<script lang="js">
-    export default {
-        name: 'Type',
-        data(){
-            return{
-                type:'-',   //'-'表示支出，'+'表示收入
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+    @Component
+    export default class Type extends Vue {
+        type = '-';     //'-'表示支出，'+'表示收入
+
+        selectType(type: string) {
+            if (type !== '-' && type !== '+') {
+                throw new Error('type不存在');
             }
-        },
-        methods:{
-            selectType(type){
-               this.type=type
-            }
+            this.type = type;
         }
-    };
+    }
+
 </script>
 
 <style lang="scss" scoped>
-    .types{
-        background:#F9B3CB;
-        color:white;
-        display:flex;
+    .types {
+        background: #F9B3CB;
+        color: white;
+        display: flex;
         text-align: center;
-        font-size:24px;
-        > li{
-            width:50%;
-            padding:12px 0;
-            display:flex;
+        font-size: 24px;
+
+        > li {
+            width: 50%;
+            padding: 12px 0;
+            display: flex;
             justify-content: center;
             align-items: center;
-            position:relative;
-            &.selected::after{
-                content:'';
-                display:block;
-                position:absolute;
-                background:#F66093;
-                width:100%;
-                bottom:0;
-                left:0;
-                height:4px;
+            position: relative;
+
+            &.selected::after {
+                content: '';
+                display: block;
+                position: absolute;
+                background: #F66093;
+                width: 100%;
+                bottom: 0;
+                left: 0;
+                height: 4px;
             }
         }
     }
